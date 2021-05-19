ac69535394416447863342f2942f2e224086b2bc
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-05-19 18:27:31 +0200

    api tutti i film

---
+++ app/Http/Controllers/Api/MovieController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\movie;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movies = movie::all();
+
+        return response()->json($movies);
+    }
+}
